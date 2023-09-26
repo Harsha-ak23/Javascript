55cb985683bb810d01234d36ce99c25d2cf1ea1f
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-09-26 09:20:16 +0000

    container config done

---
+++ 00_basics/002_variable.js
@@ -0,0 +1,11 @@
+const accountId = 245;
+let emailId = "kasaudhanak96@gmail.com"
+var password = "Ashish@123"
+village = "noida sector 58"
+let rollNo;
+// accountId = 243;
+emailId = "kasaudhanak97@gmail.com"
+password = "Ashish@345"
+village = "Rampur Bhagan"
+
+console.table([accountId, emailId, password, village, rollNo]);
---
+++ 00_basics/003_datatype.js
@@ -0,0 +1,10 @@
+let studentName = "Ashish"
+const studentId = 22
+let isIntheClass = false
+
+console.log(typeof studentId)
+console.log(typeof studentName)
+console.log(typeof isIntheClass)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof int)
