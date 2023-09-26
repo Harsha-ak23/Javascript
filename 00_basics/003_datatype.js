645cd3b0f697110fc79f4bd3bec85ced75ccba46
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-09-26 09:55:58 +0000

    conversion of datatypes

--- 00_basics/003_datatype.js
+++ 00_basics/003_datatype.js
@@ -1,10 +1,13 @@
-let studentName = "Ashish"
-const studentId = 22
-let isIntheClass = false
+// let studentName = "Ashish"
+// const studentId = 22
+// let isIntheClass = false
 
-console.log(typeof studentId)
-console.log(typeof studentName)
-console.log(typeof isIntheClass)
-console.log(typeof null)
-console.log(typeof undefined)
-console.log(typeof int)
+// console.log(typeof studentId)
+// console.log(typeof studentName)
+// console.log(typeof isIntheClass)
+// console.log(typeof null)
+// console.log(typeof undefined)
+// console.log(typeof int)
+let islogIn = 0.1;
+let booleanislogIn = Boolean(islogIn);
+console.log(booleanislogIn);
