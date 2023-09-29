3743f265f5ef8aae39fadbba1831f93911572d78
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-09-29 11:48:39 +0000

    date and time and its methods

---
+++ 00_basics/006_date_time.js
@@ -0,0 +1,25 @@
+// const mydate  = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toDateString());
+// let myCreatedDate = new Date(2023, 8, 29);
+// console.log(myCreatedDate.toLocaleString());
+// let myDate1 = new Date(2023, 8, 29, 16, 14);
+// console.log(myDate1.toLocaleString());
+// console.log(myDate1.toDateString());
+// let myDate2 = new Date("2023-09-29 16:14:00");
+// console.log(myDate2.toLocaleString())
+// let myTime = Date.now()
+// console.log(myTime);
+let myDate3 = new Date()
+// console.log(myDate3.getTime());
+// console.log(Math.floor(((Math.floor(Date.now()/1000))-(Math.floor(myDate3.getTime()/1000)))/3600));
+console.log(myDate3.getDate());
+console.log(myDate3.getHours());
+console.log(myDate3.getMonth()+1);
+console.log(myDate3.getFullYear());
+console.log(myDate3.getMinutes());
+console.log(myDate3.getDay()+1);
+console.log(myDate3.getSeconds());
+console.log(myDate3.getTime());
