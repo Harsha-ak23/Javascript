8b724eedb2fb76321f6a8c9738c8acae37aa3cb5
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-10-02 17:32:49 +0000

    basic array and its method

---
+++ 01_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const myArr = [0,1,2,3,true, "Ashish"]
+// console.log(myArr);
+
+// methods
+
+// myArr.push(6)
+// myArr.pop()
+
+//myArr.unshift(8)
+// myArr.shift()
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(8));
+
+// const newArr = myArr.join()
+// console.log(myArr) 
+// console.log(newArr) 
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2)
+
+console.log("C", myArr)
