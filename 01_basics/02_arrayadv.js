936c745e60c6321416fe77706bdb0aa36cfc8132
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-10-03 13:22:39 +0000

    element conveting into array

--- 01_basics/02_arrayadv.js
+++ 01_basics/02_arrayadv.js
@@ -3,5 +3,16 @@ const dc_heros = ["superman", "flash", "batman"]
 // marval_heros.push(dc_heros)
 // const myn1 = marval_heros.concat(dc_heros)
 // console.log(myn1)
-const all_new_heros = [...marval_heros, ...dc_heros];
-console.log(all_new_heros);
+// const all_new_heros = [...marval_heros, ...dc_heros];
+// console.log(all_new_heros);
+// const myar = [1,2,[3,4],5,6,7,[[23,354,55,66,5],57,6,7],6,8,9];
+// const newarr = myar.flat(Infinity);
+// console.log(newarr);
+// console.log(Array.isArray("Ashish"));
+// console.log(Array.from("Ashish"));
+// console.log(Array.from({name:"Ashish"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
