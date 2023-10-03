6ab03b4f84e04d510cc26da90968160bb5a3e266
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-10-03 16:58:38 +0000

    check key is present or not

--- 01_basics/04_objectadv.js
+++ 01_basics/04_objectadv.js
@@ -22,6 +22,8 @@ tinderUser.isloggedIn = false;
 // const obj3 = {...obj1, ...obj2, ...obj4}
 // console.log(obj3)
 // console.log(obj1)
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty('isloggedIn'));
+console.log(tinderUser.hasOwnProperty('islogged'));
