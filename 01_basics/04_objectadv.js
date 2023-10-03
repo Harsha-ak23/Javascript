bffdb367b94027895fa0e7dcfc27b32c3854e39b
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-10-03 15:14:10 +0000

    advance object method

---
+++ 01_basics/04_objectadv.js
@@ -0,0 +1,27 @@
+const tinderUser = new Object()
+tinderUser.name = "Ashish";
+tinderUser.id = "123abc";
+tinderUser.isloggedIn = false;
+// console.log(tinderUser)
+// const regularUser = {
+//     email: "kasaudhanak@gmail.com",
+//     fullname:{
+//         username:{
+//             firstname: "Harsh",
+//             lastname: "Kasaudhan"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname.username.firstname);
+// const obj1 = {1:"a", 2:"b"};
+// const obj2 = {3:"c", 4:"d"};
+// const obj4 = {5:"e", 6:"f"}
+// console.log(obj1)
+// const obj3 = {obj1, obj2, obj4}
+// const obj3 = Object.assign({},obj1, obj2, obj4);
+// const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3)
+// console.log(obj1)
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
