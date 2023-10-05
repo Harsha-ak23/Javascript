f253eba7b7f2aa5cb70f4da87cad0744b9d12d20
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-10-05 14:42:09 +0000

    scope in javascript

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,18 @@
+// let a = 300
+// const b = 400
+// var c = 500
+// {
+//     let a = 20
+//     const b = 59 
+//     var c = 50
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+ 
+const addTwo = function(num){
+    return num +2
+}
+
+console.log(addTwo(5));
