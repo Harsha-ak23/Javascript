82813e31db94915b51084f890261d9b04c752c6b
Harsha-ak23 <145993720+Harsha-ak23@users.noreply.github.com>
2023-10-06 21:29:20 +0000

    IIFE deceleration

---
+++ 02_basics/04_IIFEfunction.js
@@ -0,0 +1,10 @@
+// (function chai(){
+//     console.log(`DB is connected`);
+// })();
+// (()=> {
+//     console.log(`DB connected`);
+// })();
+
+((name)=>{
+    console.log(`my name is ${name}`)
+})("Ashish");
